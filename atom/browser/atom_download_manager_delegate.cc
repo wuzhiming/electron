4e4275eae886192b3aedf088c22ce47b42e4dc71
Shelley Vohr <shelley.vohr@gmail.com>
2019-02-19 14:53:27 +0000

    update js and fix build

--- atom/browser/atom_download_manager_delegate.cc
+++ atom/browser/atom_download_manager_delegate.cc
@@ -121,17 +121,14 @@ void AtomDownloadManagerDelegate::OnDownloadPathGenerated(
     settings.force_detached = offscreen;
 
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Local<v8::Context> context = isolate->GetCurrentContext();
     scoped_refptr<atom::util::Promise> dialog_promise =
         new atom::util::Promise(isolate);
+    auto dialog_callback =
+        base::Bind(&AtomDownloadManagerDelegate::OnDownloadSaveDialogDone,
+                   base::Unretained(this), download_id, callback);
 
     file_dialog::ShowSaveDialog(settings, dialog_promise);
-    ignore_result(dialog_promise->GetHandle()->Then(
-        context,
-        v8::Local<v8::Function>::Cast(mate::ConvertToV8(
-            isolate,
-            base::Bind(&AtomDownloadManagerDelegate::OnDownloadSaveDialogDone,
-                       callback, base::Unretained(this), download_id)))));
+    ignore_result(dialog_promise->Then(dialog_callback));
   } else {
     callback.Run(path, download::DownloadItem::TARGET_DISPOSITION_PROMPT,
                  download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS, path,
